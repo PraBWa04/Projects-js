496f941ccb520dbb10d83d98a24800bba745f3df
Ivan Matyshchak <93609810+PraBWa04@users.noreply.github.com>
2024-06-12 17:54:31 +0200

    Add files via upload

---
+++ Calculator/calculator.js
@@ -0,0 +1,56 @@
+const resultElement = document.getElementById("result");
+const input1Element = document.getElementById("input1");
+const input2Element = document.getElementById("input2");
+const submitBtn = document.getElementById("submit");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const divisionBtn = document.getElementById("division");
+const multiplicationBtn = document.getElementById("multiplication");
+let action = "+";
+
+plusBtn.onclick = function () {
+  action = "+";
+};
+
+minusBtn.onclick = function () {
+  action = "-";
+};
+
+divisionBtn.onclick = function () {
+  action = "/";
+};
+
+multiplicationBtn.onclick = function () {
+  action = "*";
+};
+
+function printResult(result) {
+  if (result < 0) {
+    resultElement.style.color = "red";
+  } else {
+    resultElement.style.color = "green";
+  }
+  resultElement.textContent = result;
+}
+
+function computeNumberWithAction(val1, val2, actionSymbol) {
+  const num1 = Number(val1.value);
+  const num2 = Number(val2.value);
+  if (actionSymbol == "+") {
+    return num1 + num2;
+  }
+  if (actionSymbol == "-") {
+    return num1 - num2;
+  }
+  if (actionSymbol == "/") {
+    return num1 / num2;
+  }
+  if (actionSymbol == "*") {
+    return num1 * num2;
+  }
+}
+
+submitBtn.onclick = function () {
+  const result = computeNumberWithAction(input1, input2, action);
+  printResult(result);
+};
